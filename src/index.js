2e1a997f4450840a9820def4926425d0f90aa944
fh <feih@angelcomm.cn>
2021-04-14 17:10:30 +0800

    更新手写loader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import { add } from './util/add.js';
-import multiply  from './util/multiply.js';
+import multiply from './util/multiply.js';
+import './style/theme.less';
+import './style/index.css';
 
+console.log(1);
 const a = add(1, 4);
 const b = multiply(2, 4);
 
+console.log(2);
+
 document.body.innerHTML = `<div>add: ${a}</div><div>multiply: ${b}</div>`;
+console.log(3);
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,22 @@ module.exports = {
     output: {
         path: path.join(__dirname, './dist'),
         filename: 'bundle.js'
-    }
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: [
+                    path.resolve(__dirname, './lib/loader/drop-console.js')
+                ]
+            },
+            {
+                test: /\.(c|le)ss$/,
+                use: [
+                    path.resolve(__dirname, './lib/loader/style-loader.js'),
+                    path.resolve(__dirname, './lib/loader/less-loader.js')
+                ]
+            }
+        ]
+    },
 };
